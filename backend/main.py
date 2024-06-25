b65891fbb39d881b8913b950abac774670447826
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-06-26 03:15:29 +0800

    idk if this works

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ import models
 import schema
 from database import SessionLocal, engine
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request, JSONResponse
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
@@ -19,6 +19,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+@app.api_route("/{path_name:path}", methods=["GET"])
+async def catch_all_get(req: Request, path_name: str):
+    
+    raise HTTPException(status_code = 403, details = "User sent a GET Request")
+
+    return JSONResponse(
+        status_code = 403,
+        content={"message": "yeah the server is working, just not for you"},
+    )
+
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
