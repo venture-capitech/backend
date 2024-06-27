1ad1d30be2ca813c3655acb23e9d31cf1969f6a7
Wei Ming <alvinwm0523@gmail.com>
2024-06-27 20:40:19 +0800

    Version 1.1
    Added app.get to read user

--- backend/main.py
+++ backend/main.py
@@ -29,3 +29,11 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
